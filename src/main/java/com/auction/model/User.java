6e84e49d2def6c6cc85b5b0606bfb2fcde6fcb62
hungntils <110363045+hungntils@users.noreply.github.com>
2026-04-04 19:33:40 +0700

    Add email field and constructor to User class

--- src/main/java/com/auction/model/User.java
+++ src/main/java/com/auction/model/User.java
@@ -3,4 +3,11 @@ package com.auction.model;
 public abstract class User extends Entity {
     private String username;
     private String password;
+    private String email;
+    public User(String id, String username, String email, String password) {
+        super(id); 
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 }
