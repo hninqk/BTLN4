2d8b31cff2c38cf31f2b9dc46cfa1f887463f066
kminh <71207280+hninqk@users.noreply.github.com>
2026-04-04 17:13:19 +0700

    delete the unused file

--- src/main/java/com/auction/app/Launcher.java
+++
@@ -1,8 +0,0 @@
-package com.auction.app;
-
-public class Launcher {
-    public static void main(String[] args) {
-        // This tricks Java into starting without checking for JavaFX modules first
-        AdminServerGUI.main(args);
-    }
-}
