fc408205992d0baa17f865c0011eb5a6b4dde0f9
hungntils <110363045+hungntils@users.noreply.github.com>
2026-04-05 10:57:00 +0700

    Refactor User constructor to use default ID

--- src/main/java/com/auction/model/User.java
+++ src/main/java/com/auction/model/User.java
@@ -1,11 +1,21 @@
 package com.auction.model;
 
+import java.time.LocalDateTime;
+
 public abstract class User extends Entity {
     private String username;
     private String password;
     private String email;
-    public User(String id, String username, String email, String password) {
-        super(id); 
+
+    public User(String username, String email, String password) {
+        super(); 
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String id, LocalDateTime createdAt, String username, String email, String password) {
+        super(id, createdAt); 
         this.username = username;
         this.email = email;
         this.password = password;
