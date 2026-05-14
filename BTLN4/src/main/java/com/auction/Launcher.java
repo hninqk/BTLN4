b021b1c07b5045d438658b3e332021ddf23a82fd
hninqk <stephenwhawking34@gmail.com>
2026-05-14 23:03:26 +0700

    chore: actually fix Launcher.java merge markers

--- BTLN4/src/main/java/com/auction/Launcher.java
+++ BTLN4/src/main/java/com/auction/Launcher.java
@@ -1,15 +1,7 @@
 package com.auction;
 
 public class Launcher {
-<<<<<<< HEAD
     public static void main(String[] args) {
         Main.main(args);
     }
 }
-=======
-  public static void main(String[] args) {
-    // Gọi thẳng vào class Main của JavaFX
-    Main.main(args);
-  }
-}
->>>>>>> dev
